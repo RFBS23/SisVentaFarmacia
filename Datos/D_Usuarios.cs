3f2b64d604bcc457d8eb98b11ec057356987eee1
RFBS18 <fabriziobarrios22@gmail.com>
2023-08-09 20:33:33 -0500

    capa datos

---
+++ Datos/D_Usuarios.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Entidad;
+using System.Data.SqlClient;
+using System.Data;
+
+
+namespace Datos
+{
+    public class D_Usuarios
+    {
+        public List<Usuario> Listar()
+        {
+            List<Usuario> lista = new List<Usuario>();
+
+            try
+            {
+                using (SqlConnection oconexion = new SqlConnection(Conexion.cndb)) {
+                    string query = "select idusuario, nombreusuario, correo, clave, restablecer, estado from usuarios";
+                    SqlCommand cmd = new SqlCommand(query, oconexion);
+                    cmd.CommandType = CommandType.Text;
+                    oconexion.Open();
+
+                    using (SqlDataReader dr = cmd.ExecuteReader() )
+                    {
+                        while (dr.Read())
+                        {
+                            lista.Add(new Usuario() {
+                                idusuario = Convert.ToInt32(dr["idusuario"]),
+                                nombreusuario = dr["nombreusuario"].ToString(),
+                                correo = dr["correo"].ToString(),
+                                clave = dr["clave"].ToString(),
+                                restablecer = Convert.ToBoolean(dr["restablecer"]),
+                                estado = Convert.ToBoolean(dr["estado"])
+                            });
+                        }
+                    }
+                }
+            } catch
+            {
+                lista = new List<Usuario>();
+            }
+
+            return lista;
+        }
+
+        //procedimiento registrar
+        public int Registrar(Usuario obj, out string Mensaje)
+        {
+            int idautogenerado = 0;
+            Mensaje = string.Empty;
+            try
+            {
+                using (SqlConnection oconexion = new SqlConnection(Conexion.cndb))
+                {
+                    SqlCommand cmd = new SqlCommand("spu_registrarusuario_user", oconexion);
+                    cmd.Parameters.AddWithValue("nombreusuario", obj.nombreusuario);
+                    cmd.Parameters.AddWithValue("correo", obj.correo);
+                    cmd.Parameters.AddWithValue("clave", obj.clave);
+                    cmd.Parameters.AddWithValue("estado", obj.estado);
+                    cmd.Parameters.Add("resultado", SqlDbType.Int).Direction = ParameterDirection.Output;
+                    cmd.Parameters.Add("mensaje", SqlDbType.VarChar, 60).Direction = ParameterDirection.Output;
+                    cmd.CommandType = CommandType.StoredProcedure;
+
+                    oconexion.Open();
+                    cmd.ExecuteNonQuery();
+                    idautogenerado = Convert.ToInt32(cmd.Parameters["resultado"].Value);
+                    Mensaje = cmd.Parameters["mensaje"].Value.ToString();
+                }
+            } catch (Exception ex) 
+            {
+                idautogenerado = 0;
+                Mensaje = ex.Message;
+            } 
+            return idautogenerado;
+        }
+
+        //procedimiento editar
+        public bool Editar(Usuario obj, out string Mensaje)
+        {
+            bool resultado = false;
+            Mensaje = string.Empty;
+            try
+            {
+                using (SqlConnection oconexion = new SqlConnection(Conexion.cndb))
+                {
+                    SqlCommand cmd = new SqlCommand("spu_editarusuario_user", oconexion);
+                    cmd.Parameters.AddWithValue("idusuario", obj.idusuario);
+                    cmd.Parameters.AddWithValue("nombreusuario", obj.nombreusuario);
+                    cmd.Parameters.AddWithValue("correo", obj.correo);
+                    cmd.Parameters.AddWithValue("estado", obj.estado);
+                    cmd.Parameters.Add("resultado", SqlDbType.Bit).Direction = ParameterDirection.Output;
+                    cmd.Parameters.Add("mensaje", SqlDbType.VarChar, 60).Direction = ParameterDirection.Output;
+                    cmd.CommandType = CommandType.StoredProcedure;
+                    oconexion.Open();
+                    cmd.ExecuteNonQuery();
+                    resultado = Convert.ToBoolean(cmd.Parameters["resultado"].Value);
+                    Mensaje = cmd.Parameters["mensaje"].Value.ToString();
+                }
+            } catch (Exception ex)
+            {
+                resultado = false;
+                Mensaje = ex.Message;
+            }
+            return resultado;
+        }
+
+        //eliminar
+        public bool Eliminar(int id, out string Mensaje)
+        {
+            bool resultado = false;
+            Mensaje = string.Empty;
+            try
+            {
+                using (SqlConnection oconexion = new SqlConnection(Conexion.cndb))
+                {
+                    SqlCommand cmd = new SqlCommand("delete top (1) from usuario where idusuario = @id", oconexion);
+                    cmd.Parameters.AddWithValue("@id", id);
+                    cmd.CommandType = CommandType.Text;
+                    oconexion.Open();
+                    resultado = cmd.ExecuteNonQuery() > 0 ? true : false;
+                }
+            } catch (Exception ex)
+            {
+                resultado = false;
+                Mensaje = ex.Message;
+            }
+            return resultado;
+        }
+
+    }
+}
