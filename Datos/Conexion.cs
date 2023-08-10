21f0c1b4f0811d36dabdef1a22e0dd45e10eeaa7
RFBS18 <fabriziobarrios22@gmail.com>
2023-08-09 20:33:12 -0500

    Conexion de la base de datos

---
+++ Datos/Conexion.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.Configuration;
+
+namespace Datos
+{
+   public class Conexion
+    {
+        public static string cndb = ConfigurationManager.ConnectionStrings["conexion"].ToString();
+    }
+}
