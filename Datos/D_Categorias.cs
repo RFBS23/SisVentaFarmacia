87da7f409f0a2f31855cdf0e7791a80f2a968d11
RFBS18 <fabriziobarrios22@gmail.com>
2023-08-12 00:41:40 -0500

    agregando categorias

---
+++ Datos/D_Categorias.cs
@@ -0,0 +1,142 @@
+﻿using Entidad;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Entidad;
+using System.Data.SqlClient;
+using System.Data;
+
+namespace Datos
+{
+    public class D_Categorias
+    {
+        public List<Categoria> Listar()
+        {
+            List<Categoria> lista = new List<Categoria>();
+
+            try
+            {
+                using (SqlConnection oconexion = new SqlConnection(Conexion.cndb))
+                {
+                    string query = "select idcategoria, descripcion, estado from categorias";
+                    SqlCommand cmd = new SqlCommand(query, oconexion);
+                    cmd.CommandType = CommandType.Text;
+                    oconexion.Open();
+
+                    using (SqlDataReader dr = cmd.ExecuteReader())
+                    {
+                        while (dr.Read())
+                        {
+                            lista.Add(new Categoria()
+                            {
+                                idcategoria = Convert.ToInt32(dr["idcategoria"]),
+                                descripcion = dr["descripcion"].ToString(),
+                                estado = Convert.ToBoolean(dr["estado"])
+                            });
+                        }
+                    }
+                }
+            }
+            catch
+            {
+                lista = new List<Categoria>();
+            }
+
+            return lista;
+        }
+
+        //registrar
+        public int Registrar(Categoria obj, out string Mensaje)
+        {
+            int idautogenerado = 0;
+            Mensaje = string.Empty;
+            try
+            {
+                using (SqlConnection oconexion = new SqlConnection(Conexion.cndb))
+                {
+                    SqlCommand cmd = new SqlCommand("spu_registrarcategoria_categoria", oconexion);
+                    cmd.Parameters.AddWithValue("descripcion", obj.descripcion);
+                    cmd.Parameters.AddWithValue("estado", obj.estado);
+                    cmd.Parameters.Add("resultado", SqlDbType.Int).Direction = ParameterDirection.Output;
+                    cmd.Parameters.Add("mensaje", SqlDbType.VarChar, 60).Direction = ParameterDirection.Output;
+                    cmd.CommandType = CommandType.StoredProcedure;
+
+                    oconexion.Open();
+                    cmd.ExecuteNonQuery();
+                    idautogenerado = Convert.ToInt32(cmd.Parameters["resultado"].Value);
+                    Mensaje = cmd.Parameters["mensaje"].Value.ToString();
+                }
+            }
+            catch (Exception ex)
+            {
+                idautogenerado = 0;
+                Mensaje = ex.Message;
+            }
+            return idautogenerado;
+        }
+
+        //editar
+        public bool Editar(Categoria obj, out string Mensaje)
+        {
+            bool resultado = false;
+            Mensaje = string.Empty;
+            try
+            {
+                using (SqlConnection oconexion = new SqlConnection(Conexion.cndb))
+                {
+                    SqlCommand cmd = new SqlCommand("spu_editarcategoria_categoria", oconexion);
+                    cmd.Parameters.AddWithValue("idcategoria", obj.idcategoria);
+                    cmd.Parameters.AddWithValue("descripcion", obj.descripcion);
+                    cmd.Parameters.AddWithValue("estado", obj.estado);
+                    cmd.Parameters.AddWithValue("estado", obj.estado);
+                    cmd.Parameters.Add("resultado", SqlDbType.Bit).Direction = ParameterDirection.Output;
+                    cmd.Parameters.Add("mensaje", SqlDbType.VarChar, 100).Direction = ParameterDirection.Output;
+                    cmd.CommandType = CommandType.StoredProcedure;
+                    oconexion.Open();
+                    cmd.ExecuteNonQuery();
+                    resultado = Convert.ToBoolean(cmd.Parameters["resultado"].Value);
+                    Mensaje = cmd.Parameters["mensaje"].Value.ToString();
+                }
+            }
+            catch (Exception ex)
+            {
+                resultado = false;
+                Mensaje = ex.Message;
+            }
+            return resultado;
+        }
+
+        //eliminar
+        public bool Eliminar(int id, out string Mensaje)
+        {
+            bool resultado = false;
+            Mensaje = string.Empty;
+            try
+            {
+                using (SqlConnection oconexion = new SqlConnection(Conexion.cndb))
+                {
+                    SqlCommand cmd = new SqlCommand("spu_eliminarcategoria_categoria", oconexion);
+                    cmd.Parameters.AddWithValue("idcategoria", id);
+                    cmd.Parameters.Add("resultado", SqlDbType.Bit).Direction = ParameterDirection.Output;
+                    cmd.Parameters.Add("mensaje", SqlDbType.VarChar, 60).Direction = ParameterDirection.Output;
+                    cmd.CommandType = CommandType.StoredProcedure;
+                    oconexion.Open();
+                    cmd.ExecuteNonQuery();
+                    resultado = Convert.ToBoolean(cmd.Parameters["resultado"].Value);
+                    Mensaje = cmd.Parameters["mensaje"].Value.ToString();
+                }
+            }
+            catch (Exception ex)
+            {
+                resultado = false;
+                Mensaje = ex.Message;
+            }
+            return resultado;
+        }
+    }
+}
