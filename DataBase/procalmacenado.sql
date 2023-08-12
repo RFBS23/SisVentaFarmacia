7fd6c326cc006885aee35e0ab8a47ff8b14dfe49
RFBS18 <fabriziobarrios22@gmail.com>
2023-08-12 00:41:13 -0500

    agregando procedimineto almacenado de categorias

--- DataBase/procalmacenado.sql
+++ DataBase/procalmacenado.sql
@@ -50,3 +50,67 @@ begin
 	set @mensaje = 'El correo ya se encuentra registrado'
 end
 go
+
+--categoria
+create proc spu_registrarcategoria_categoria(
+	@descripcion varchar(100),
+	@estado bit,
+	@mensaje varchar(60) output,
+	@resultado int output
+)
+as
+begin
+	set @resultado = 0
+	if not exists (select * from categorias where descripcion = @descripcion)
+	begin
+		insert into categorias(descripcion, estado) values (@descripcion, @estado)
+		set @resultado = SCOPE_IDENTITY()
+	end
+	else
+	set @mensaje = 'La Categoria ya existe'
+end
+go
+
+create proc spu_editarcategoria_categoria(
+	@idcategoria int,
+	@descripcion varchar(100),
+	@estado bit,
+	@mensaje varchar(100) output,
+	@resultado int output
+)
+as
+begin
+	set @resultado = 0
+	if not exists (select * from categorias where descripcion = @descripcion and idcategoria != @idcategoria)
+	begin
+		update top (1) categorias set
+		descripcion = @descripcion,
+		estado = @estado
+		where idcategoria = @idcategoria
+		set @resultado = 1
+	end
+	else
+	set @mensaje = 'La categoria ya existe'
+end
+go
+
+create proc spu_eliminarcategoria_categoria(
+	@idcategoria int,
+	@mensaje varchar(60) output,
+	@resultado bit output
+)
+as
+begin
+	set @resultado = 0
+	if not exists (select * from productos p inner join categorias c on c.idcategoria = p.idcategoria
+					where p.idcategoria = @idcategoria)
+	begin
+		delete top (1) from categorias where idcategoria = @idcategoria
+		set @resultado = 1
+	end
+	else
+	set @mensaje = 'La Categoria Tiene un Producto Relacionado'
+end
+go
+
+select * from categorias;
