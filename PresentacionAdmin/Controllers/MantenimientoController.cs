f2e045c8fc5c7db99dad74f45f439041f6735e1c
RFBS18 <fabriziobarrios22@gmail.com>
2023-08-12 00:43:32 -0500

    agergando vista mantenimineto y arreglando views de categoria

--- PresentacionAdmin/Controllers/MantenimientoController.cs
+++ PresentacionAdmin/Controllers/MantenimientoController.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Entidad;
+using Negocios;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,6 +10,10 @@ namespace PresentacionAdmin.Controllers
 {
     public class MantenimientoController : Controller
     {
+        public ActionResult Mantenimientos()
+        {
+            return View();
+        }
         // GET: Mantenimiento
         public ActionResult Categorias()
         {
@@ -22,6 +28,42 @@ namespace PresentacionAdmin.Controllers
         public ActionResult Productos()
         {
             return View();
+        }
+
+        [HttpGet]
+        public JsonResult ListarCategorias()
+        {
+            List<Categoria> oLista = new List<Categoria>();
+            oLista = new N_Categorias().Listar();
+            return Json(new { data = oLista }, JsonRequestBehavior.AllowGet);
+        }
+
+        [HttpPost]
+        public JsonResult GuardarCategorias(Categoria objeto)
+        {
+            object resultado;
+            string mensaje = string.Empty;
+
+            if (objeto.idcategoria == 0)
+            {
+                resultado = new N_Categorias().Registrar(objeto, out mensaje);
+            }
+            else
+            {
+                resultado = new N_Categorias().Editar(objeto, out mensaje);
+            }
+
+            return Json(new { resultado = resultado, mensaje = mensaje }, JsonRequestBehavior.AllowGet);
+        }
+
+        [HttpPost]
+        public JsonResult EliminarCategorias(int id)
+        {
+            bool respuesta = false;
+            string mensaje = string.Empty;
+
+            respuesta = new N_Categorias().Eliminar(id, out mensaje);
+            return Json(new { resultado = respuesta, mensaje = mensaje }, JsonRequestBehavior.AllowGet);
         }
     }
 }
